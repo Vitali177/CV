8c3d1f9e0ff28e668fa2845432da01e6120bdb09
Vitali <vskrabatun@gmail.com>
2020-02-04 10:41:45 +0300

    feat: add slider animation

--- assets/scripts/slider.js
+++ assets/scripts/slider.js
@@ -1,55 +1,89 @@
 let slideIndex = 1,
   slides = document.querySelectorAll('.slider__item'),
+  slideContainer = document.querySelector('.slider__container'),
   prev = document.querySelector('.prev'),
   next = document.querySelector('.next'),
   dotsWrap = document.querySelector('.slider__dots'),
-  dots = document.querySelectorAll('.dot'),
-  slidesTitles = document.querySelectorAll('.slider__title');
+  dots = document.querySelectorAll('.dot');
 
-  function showSlides(n) {
+const size = slides[0].clientWidth;
+const distanceNearSlides = 100;
 
-    if (n > slides.length) {
-      slideIndex = 1;
-      n = 1;
-    }
-    if (n < 1) {
-      slideIndex = slides.length;
-    }
+slideContainer.style.width = `${size * slides.length + distanceNearSlides * (slides.length - 1)}px`;
+slideContainer.style.transform = `translateX(${(-size * slideIndex) - distanceNearSlides}px)`;
 
-    slides.forEach((slide) => slide.style.display = 'none');
-    dots.forEach((dot) => dot.classList.remove('dot_active'));
-    slidesTitles.forEach((slideTitle) => slideTitle.style.display = 'none');
 
-    slides[slideIndex - 1].style.display = 'block';
+next.addEventListener('click', (e) => { 
+  changeSlide(e);
+});
+
+prev.addEventListener('click', (e) => { 
+  changeSlide(e);
+});
+
+
+
+slideContainer.addEventListener('transitionend', () => {
+  if (slides[slideIndex].id === 'last-clone') {
+    slideIndex = slides.length - 2;
+    slideContainer.style.transform = `translateX(${-((size + distanceNearSlides) * slideIndex)}px)`;
+  }
+
+  if (slides[slideIndex].id === 'first-clone') {
+    slideContainer.style.transition = 'none';
+    slideIndex = slides.length - slideIndex;
+    slideContainer.style.transform = `translateX(${-((size + distanceNearSlides) * slideIndex)}px)`;
+  }
+});
+
+function changeSlide(e) {
+  if (slideIndex <= 0) {
+    slideIndex = 1;
+  }
+
+  if (slideIndex >= slides.length - 1) {
+    slideIndex = -1;
+  }
+
+  if (e.target.closest('div').classList.contains('next')) {
+    slideIndex++;
+  } else {
+    slideIndex--;
+  }
+
+  slideContainer.style.transition = 'transform 0.4s ease-in-out';
+  slideContainer.style.transform = `translateX(${-((size + distanceNearSlides) * slideIndex)}px)`;
+  selectDot();
+}
+
+
+function selectDot() {  
+  dots.forEach((dot) => dot.classList.remove('dot_active'));
+
+  if (slideIndex === 0) { // last-clone
+    dots[dots.length - 1].classList.add('dot_active');  // select last dot
+  } else if (slideIndex === slides.length - 1) {  // first-clone
+    dots[0].classList.add('dot_active');  // select first dot
+  } else {
     dots[slideIndex - 1].classList.add('dot_active');
-    slidesTitles[slideIndex - 1].style.display = 'block';
   }
+}
 
-  function plusSlides(n) {
-    showSlides(slideIndex += n);
-  }
 
-  function currentSlide(n) {
-    showSlides(slideIndex = n);
-  } 
+function showSlideFromDot(n) {
+  slideIndex = n;
 
-  prev.addEventListener('click', () => {
-    plusSlides(-1);
-  });
+  dots.forEach((dot) => dot.classList.remove('dot_active'));
+  dots[n - 1].classList.add('dot_active');
 
-  next.addEventListener('click', () => {
-    plusSlides(1);
-  });
+  slideContainer.style.transition = 'transform 0.4s ease-in-out, opacity .3s';
+  slideContainer.style.transform = `translateX(${-((size + distanceNearSlides) * slideIndex)}px)`;
+}
 
-  dotsWrap.addEventListener('click', (e) => {
-    for (let i = 0; i < dots.length + 1; i++) {
-      if (e.target.classList.contains('dot') && e.target === dots[i-1]) {
-        currentSlide(i);
-      }
+dotsWrap.addEventListener('click', (e) => {
+  for (let i = 0; i < dots.length + 1; i++) {
+    if (e.target.classList.contains('dot') && e.target === dots[i-1]) {
+      showSlideFromDot(i);
     }
-  });
-
-
-  window.addEventListener('DOMContentLoaded', () => {
-    showSlides(slideIndex);
-  });
+  }
+});
